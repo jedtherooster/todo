6e5a23fe777d769641d29bd5453170acc1f00768
jedtherooster <55644921+jedtherooster@users.noreply.github.com>
2025-05-01 13:51:24 +1200

    done

--- index.js
+++ index.js
@@ -5,18 +5,31 @@ const app = express();
 const port = 3100;
 
 let tasks = [];
+let postId = 0;
 
 app.use(bodyParser.urlencoded());
 app.use(express.static('public'));
 
 app.get("/", (req, res) => {
+
     res.render("index.ejs", {
         tasks: tasks,
     });
 });
 
 app.get("/delete/:id", (req, res) => {
-    
+    postId = req.params.id;
+
+    if (!tasks[postId]) {
+        return res.status(404).send("Post not found");
+    }
+
+    tasks.splice(postId, 1);
+
+    res.render("index.ejs", {
+        postId: postId,
+        tasks: tasks,
+    });
 });
 
 app.post("/submit", (req, res) => {
